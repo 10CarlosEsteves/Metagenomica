15b76db88fd24cdc5d5a513a7b1be0fded6fe812
Carlos Henrique Miranda Esteves <101829419+10CarlosEsteves@users.noreply.github.com>
2022-10-05 20:01:29 -0300

    Create Function.py

---
+++ Function.py
@@ -0,0 +1,98 @@
+import os
+from pysradb.sraweb import SRAweb
+from pysradb.search import SraSearch
+
+os.system("export PATH=$PATH:$HOME/.local/bin")
+os.system("export PATH=$PATH:$PWD/sratoolkit.3.0.0-ubuntu64/bin")
+
+
+#Função de decisão
+def decisao():
+    print("_____________________________________________________________________________")
+    print("\nGostaria de voltar ao menu ou encerrar o programa?")
+    print("[0] Voltar ao menu | [5] Encerrar programa")
+    return int(input(">"))
+
+
+
+#Função de pesquisa geral no SRA/NCBI
+def SRApesquisar():
+    print("_____________________________________________________________________________")
+    query = str(input("Insira a sua pesquisa: "))
+    print("RESULTADOS ENCONTRADOS:\n")
+    instance = SraSearch(2,20,query)
+    instance.search()
+    df = instance.get_df()
+    print(df)
+
+
+
+#Função de retorno nulo
+def default(argumento):
+    if argumento=="" or argumento=="None":
+        return None
+
+    else:
+        return argumento
+
+
+
+#Função de Pesquisa detalhada no SRA/NCBI
+def SRAdetalhado_pesquisar():
+    print("_____________________________________________________________________________")
+    print("INSTRUÇÕES: O programa fará varias perguntas para personalizar a busca. Caso")
+    print("voce não queira responder uma pergunta aperte enter ou digite None, e a")
+    print("pergunta será ignorada e será passada para próxima pergunta. Caso este comando")
+    print("retorne um erro ou retorne nada encontrado, por favor ,verifique a ortografia")
+    print("e tente novamente")
+    print("_____________________________________________________________________________")
+
+    maximo=str(input("Digite o máximo de resultados a ser retornado: "))
+
+    pergunta=str(input("Digite a sua pesquisa: "))
+    maximo=default(maximo)
+
+    organismo=str(input("Digite o organismo de preferencia: "))
+    maximo=default(maximo)
+
+    plataforma=str(input("Digite a plataforma usada no sequenciamento(illumina, ion torrent, oxford nanopore): "))
+    maximo=default(maximo)
+
+    fonte=str(input("Digite a biblioteca fonte(genomic, metagenomic, transcriptomic) :"))
+    maximo=default(maximo)
+
+    data = str(input("Digite uma data específica(dd-mm-yyyy): "))
+    maximo=default(maximo)
+
+    estrategia = str(input("Digite a estratégia(wgs, amplicon, rna seq): "))
+    maximo=default(maximo)
+
+    print("RESULTADOS ENCONTRADOS:\n")
+
+    instance=SraSearch(2, maximo, query= pergunta, organism=organismo, platform=plataforma, source=fonte ,publication_date=data, strategy=estrategia)
+    instance.search()
+    df = instance.get_df()
+    print(df)
+
+
+
+#Função de consulta dos metadados
+def metadados():
+    print("_____________________________________________________________________________")
+    codigo=str(input("Por favor, digite o código SRA do NCBI para consultarmos os metadados: "))
+    print("RESULTADOS ENCONTRADOS:\n")
+    db = SRAweb()
+    df = db.sra_metadata(codigo)
+    df
+    print(df)
+
+
+
+#Função de downloads
+def download():
+    print("_____________________________________________________________________________")
+    codigo= str(input("Por favor, para começar o download, insira código SRA do NCBI: "))
+    fastq = str("fasterq-dump "+codigo+" --split-files")
+    print("Download ocorrendo. Isto pode demorar um pouco...")
+    print("RESULTADO DO PROGRAMA:\n")
+    os.system(fastq)
